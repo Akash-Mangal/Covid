d844eca5ff9126daeda3c60016a3a994291fa9cd
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-05-29 23:11:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from report import *
 
 
 st.set_page_config(layout="wide")
-choice=st.cache()
+
 #st.title(PROJECT_NAME)
 with st.beta_container():
     st.write('<style>body { font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 938px;z-index: 1; background: White; color: #F63366; position:fixed;top:0px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Covid-19 Detection"+'</b></h2></div>', unsafe_allow_html=True)
