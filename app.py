d6ec229f67942ea327e6e06af60e2f2aecbe2444
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-06-05 10:24:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,35 @@ from config import *
 import pandas as pd
 import numpy as np
 import plotly.express as px
-import db
+
 import os
 from report import *
 from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+import sqlalchemy
+from datetime import datetime
 
 # setup db code
 Base = declarative_base()
+
+# create table as python class
+class Image(Base):
+    __tablename__ = 'images'
+    id = Column(Integer,primary_key=True)
+    path = Column(String)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+    contact = Column(Integer)
+    uploaded_on = Column(String,default=datetime.now().strftime(" %d %b, %Y at %I:%M %p"))
+    result=Column(String)
+
+    def __str__(self):
+        return self.path
+    
+    def __repr__(self) -> str:
+        return self.path
 
 
 st.set_page_config(layout="wide")
