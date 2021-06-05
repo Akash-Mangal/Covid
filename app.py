06293f7192a2da348b80496e46003cb65e7b341e
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-06-05 11:46:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,16 +33,20 @@ class Image(Base):
     def __repr__(self) -> str:
         return self.path
 
-
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///db.sqlite3")
+    Base.metadata.create_all(engine)
+    st.write("done")
+    
 st.set_page_config(layout="wide")
 #st.title(PROJECT_NAME)
 with st.beta_container():
     st.write('<style>body { font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 938px;z-index: 1; background: White; color: #F63366; position:fixed;top:0px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Covid-19 Detection"+'</b></h2></div>', unsafe_allow_html=True)
 
-if not os.path.exists("db.sqlite3"):
-    engine = create_engine("sqlite:///db.sqlite3")
-    Base.metadata.create_all(engine)
-    st.write("done")
+//if not os.path.exists("db.sqlite3"):
+//    engine = create_engine("sqlite:///db.sqlite3")
+//  Base.metadata.create_all(engine)
+  
 
 with st.beta_container():
     cols=st.beta_columns(6)
