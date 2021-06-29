7dbd11c4088fdd4405c68761f176e7031a585502
Akash-Mangal <78715731+Akash-Mangal@users.noreply.github.com>
2021-06-29 06:27:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
 #st.title(PROJECT_NAME)
 with st.beta_container():
-    st.write('<style>body { font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 938px;z-index: 1; background: White; color: #F63366; position:fixed;top:0px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Covid-19 Detection"+'</b></h2></div>', unsafe_allow_html=True)
+    st.write('<style>body { font-family: sans-serif;border-style: } .header{border-bottom-style: solid;padding-left:10px; padding-right: 1920px;z-index: 1; background: White; color: #F63366; position:fixed;top:0px;} .sticky { position: fixed;top: 20; } </style><div class="header" id="myHeader"><h2 style="color: #F63366;"><b>'+"Covid-19 Detection"+'</b></h2></div>', unsafe_allow_html=True)
 
 #if not os.path.exists("db.sqlite3"):
 #    engine = create_engine("sqlite:///db.sqlite3")
